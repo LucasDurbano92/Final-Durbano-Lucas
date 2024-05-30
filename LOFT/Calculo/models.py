f35461b2a2149bc27e987d8dc5594a34de80b6e1
Lucas Durbano <lucas.nc.durbano@gmail.com>
2024-05-30 14:43:17 -0300

    feat:Calculo

--- LOFT/Calculo/models.py
+++ LOFT/Calculo/models.py
@@ -4,12 +4,3 @@ from decimal import Decimal
 def precio_con_impuesto(precio):
     impuesto = Decimal('0.21') * precio
     return precio + impuesto
-
-
-
-
-    # def total_con_impuesto(self):
-    #     perimetro = self.calcular_perimetro()
-    #     total_varilla = self.varilla.precio_con_impuesto() * perimetro if self.varilla else 0
-    #     return total_varilla
-
